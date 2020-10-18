08967e33f25bc98a933911c98f0b0bc68c2918ac
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 16:04:00 -0700

    exported laptop form

---
+++ src/containers /laptops/LaptopForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class LaptopForm extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Create Charater </h3>
+            </div>
+        )
+    }
+}
+
+export default LaptopForm
+
