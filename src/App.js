b44ec67cf30cf52122c8b0c618040c612a22c1d1
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 16:03:40 -0700

    Imported LaptopForm, List and show

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import LaptopForm from './containers /laptops/LaptopForm';
+import LaptopList from './containers /laptops/LaptopList';
+import LaptopShow from './containers /laptops/LaptopShow';
+import Home from './laptops/Home';
 
 class App extends Component {
   
   render () {
     return (
-      <div className="App">
+      <Router>
         <h1> Laptops </h1>
+        <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path='/laptops' component={LaptopList}/>
+          <Route exact path='/laptops/new' component={LaptopForm}/>
+          <Route exact path='/laptops/:id' component={LaptopShow}/>
+        
+
+
+        </Switch>
       </div>
+      </Router>
     );
   }
 }
