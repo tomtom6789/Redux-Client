f01d164790d08193d86b02e2df23454822addb97
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 15:11:35 -0700

    Added laptops reducer

---
+++ src/reducers/laptopsReducer.js
@@ -0,0 +1,12 @@
+const intialState = { 
+    loading: true, 
+    laptops: []
+
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
