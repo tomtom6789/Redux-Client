0c497ee94bcaad319a98cf710c9ba7a264eaf076
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 16:04:10 -0700

    exported list

---
+++ src/containers /laptops/LaptopList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class LaptopList extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Here is list of laptop</h3>
+            </div>
+        )
+    }
+}
+
+export default LaptopList
+
+
