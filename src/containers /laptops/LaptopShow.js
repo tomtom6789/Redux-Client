7658657112d13876b6f3f6a39a3759bffc9e1064
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 16:04:24 -0700

    exported show

---
+++ src/containers /laptops/LaptopShow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class LaptopShow extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Show character</h3>
+            </div>
+        )
+    }
+}
+
+export default LaptopShow
+
