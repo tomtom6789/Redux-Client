25b97f9bd925f7744a063cba78a148dab3f4219e
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 16:04:53 -0700

    Home page created

---
+++ src/laptops/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Welcome to laptops Sale Page </h3>
+            </div>
+        )
+    }
+}
+
+export default Home
+
+
