0aa8b2e2fc5f69d2f38530439add5c8e87fec6bc
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 21:21:48 -0700

    fetches laptops

--- src/actions/laptop.js
+++ src/actions/laptop.js
@@ -1,6 +1,12 @@
-
-
-
+const BASE_URL = 'http://localhost:3001/'
+const LOADING = {type: "LOADING"}
 export const fetchLaptops = () => {
 
+    return(dispatch) => {
+        dispatch(LOADING);
+
+        fetch(BASE_URL + '/laptops')
+        .then(resp => resp.json())
+        .then(laptops => dispatch({ type: "LOAD_LAPTOPS", laptops }))
+    }
 }
