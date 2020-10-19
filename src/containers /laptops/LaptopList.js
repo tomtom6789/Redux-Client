c7e3705528edc04a1f501575bb06cce2c6b92fba
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 21:24:45 -0700

    laptop fetches installed

--- src/containers /laptops/LaptopList.js
+++ src/containers /laptops/LaptopList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import Laptopcard from '../../components/laptops/LaptopCard';
 export class LaptopList extends Component {
     render() {
         return (
             <div>
                 <h3> Here is list of laptop</h3>
+                <ul>
+                    <Laptopcard />
+                </ul>
             </div>
         )
     }
