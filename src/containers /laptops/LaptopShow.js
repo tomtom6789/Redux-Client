79c0f40f4972c104a36007805cd6eead0a6b04cf
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-19 15:00:06 -0700

    connect to store to retrive individual laptop

--- src/containers /laptops/LaptopShow.js
+++ src/containers /laptops/LaptopShow.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+// import laptopsReducer from '../../reducers/laptopsReducer'
+
 
 export class LaptopShow extends Component {
+
+    state = {
+        laptop: {}, 
+        loading: true 
+    }
+
+    componentDidMount () {
+        const laptop = this.props.laptops.find(laptop => laptop.id === parseInt(this.props.match.params.id, 10))
+        if(laptop)
+        this.setState ({
+            laptop, 
+            loading: false
+        })
+    }
+
+
+
     render() {
+        if(this.props.loading){
+            return <div>Loading...</div>
+        } 
         return (
             <div>
-                <h3> Show Laptop</h3>
+                <p> Name: {this.state.laptop.name} </p>
+                <p> Condition: {this.state.laptop.condition} </p>
+                <p> Price: ${this.state.laptop.price}</p>
             </div>
         )
     }
 }
 
-export default LaptopShow
+const mapStatetoProps = state => {
+    return {
+        laptops: state.laptopsReducer.laptops,
+        loading: state.laptopsReducer.loading
+    }
+}
+
+
+export default connect(mapStatetoProps, null)(LaptopShow)
 
