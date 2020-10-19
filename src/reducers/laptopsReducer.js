39d1e2bcde44442aeb87ac6b06eaa3521f34d32e
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-18 21:23:41 -0700

    adding laptops to state's array

--- src/reducers/laptopsReducer.js
+++ src/reducers/laptopsReducer.js
@@ -6,6 +6,10 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch(action.type) {
+        case "LOADING":
+            return {...state, loading:true }
+        case "LOAD_LAPTOPS":
+            return {...state, loading:false , laptops: action.laptops}
         default: 
             return state;
     }
