5a1b2bfcfd1031e68000d4a3d24ccce1adeffd85
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-21 14:29:39 -0700

    added about

---
+++ src/laptops/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const About = () => {
+    return (
+        <div>
+            <h4>This site uses to list laptop for sale locally</h4>
+        </div>
+    )
+}
+
+export default About;
