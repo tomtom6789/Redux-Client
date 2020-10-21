ede44b65fef76cb2d762448322c6d5c7d1235ec3
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-21 14:29:54 -0700

    added footer

---
+++ src/laptops/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const Footer = () => {
+    return (
+        <div className="Footer">
+            <p className="FooterContent"> © Copyright 2020 </p>
+        </div>
+    )
+}
+
+export default Footer;
