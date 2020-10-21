14d6e448a84afe5aaaab07e1d12131825eb4a263
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-10-21 14:28:01 -0700

    Add About and Footer as presentational component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import { connect } from 'react-redux'
 import Home from './laptops/Home';
+import About from './laptops/About';
+import Footer from './laptops/Footer'
 import NavBar from './components/styles/NavBar'
 import LaptopForm from './containers /laptops/LaptopForm';
 import LaptopList from './containers /laptops/LaptopList';
@@ -23,17 +25,19 @@ class App extends Component {
   render () {
     return (
       <Router>
-        <h1> Laptops </h1>
         <div className="App">
           <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
           <Route exact path='/laptops' component={LaptopList}/>
           <Route exact path='/laptops/new' component={LaptopForm}/>
           <Route exact path='/laptops/:id' component={LaptopShow}/>
           <Route render={(props) => <div> Page loading </div>} />
+         
         </Switch>
       </div>
+        <Footer />
       </Router>
     );
   }
